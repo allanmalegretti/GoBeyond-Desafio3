2ea293ddfc0f384c2bdad25cd37e8a523d101c00
Allan M Alegretti <allanalegretti@gmail.com>
2021-09-18 00:48:55 -0300

    estudando react

---
+++ src/components/ComParametro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props =>
+    <>
+        <h3>{props.titulo}</h3>
+        <p>{props.subtitulo}</p>
+        {/*<p>{1+2}</p> */}
+        {/*<p>{Array(10).fill(0)}</p>*/}
+        
+    </>
